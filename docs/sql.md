adc46e076af7499b95a90a5ae48a3453c5826328
hns1971 <524395609@qq.com>
2016-06-16 16:00:35 +0800

    SqlService初步改进，测试成功

---
+++ docs/sql.md
@@ -0,0 +1,4 @@
+# sql
+
+执行与sql有关的任务，sql执行按spring bean方式处理。
+
---
+++ src/main/java/com/aote/sql/SqlServer.java
@@ -0,0 +1,141 @@
+package com.aote.sql;
+
+import java.io.RandomAccessFile;
+
+import org.apache.log4j.Logger;
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.af.expression.Delegate;
+import com.af.expression.Program;
+
+@Component
+@Transactional
+public class SqlServer {
+	static Logger log = Logger.getLogger(SqlServer.class);
+
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	/**
+	 * 获取SQL语句的合计执行结果
+	 * @param name: sql语句名
+	 * @param str: sql语句执行参数
+	 * @return JSON格式Sql语句执行结果
+	 * @throws JSONException
+	 */
+	public JSONObject queryTotal(String name, String str) throws JSONException 
+	{
+		try {
+			JSONObject jo = new JSONObject();
+
+			// 获取原始sql语句
+			String sql = getSql(name);
+			
+			sql = "$" + sql;
+			
+			// 获取编译后的sql语句
+			JSONObject param = null;
+			if(str != null && !str.isEmpty()) {
+				log.debug(str);				
+				param = new JSONObject(str);
+				log.debug(param.get("condition"));
+			}
+			sql = getExecSql(sql, param);
+			
+			// 求和时，order by会导致sql错误，过滤掉order by部分。
+			sql = filterOutOrderBy(sql);
+			
+			Session session = sessionFactory.getCurrentSession();
+			JSONArray array = SqlHelper.query(session, sql);
+			jo.put("n", array.getJSONObject(0).getInt("n"));
+			return jo;
+		} catch (RuntimeException e) {
+			throw e;
+		} catch (Error e) {
+			throw e;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}	
+	
+	/**
+	 * 执行sql分页查询
+	 */
+	public JSONArray query(String name, int pageNo, int pageSize, String str) {
+		try {
+			// pageNo小于0， 纠正成1
+			if (pageNo <= 0) {
+				pageNo = 1;
+			}
+
+			// pageSize小于0，纠正成1
+			if (pageSize < 1 || pageSize > 1000) {
+				pageSize = 1000;
+			}
+
+			// 解析传递过来的对象属性
+			String sql = getSql(name);
+			sql = "$" + sql;
+			// 拿到json对象参数
+			JSONObject param = null;
+			if(str != null && !str.isEmpty()) {
+				log.debug(str);
+				param = new JSONObject(str);
+				log.debug(param.toString());
+			}
+			sql = getExecSql(sql, param);
+			Session session = sessionFactory.getCurrentSession();
+			JSONArray array = SqlHelper.query(session, sql, pageNo-1, pageSize);
+			log.debug(array.toString());
+			return array;
+		} catch (RuntimeException e) {
+			throw e;
+		} catch (Error e) {
+			throw e;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	// 过滤order by子句
+	private String filterOutOrderBy(String sql) {
+		int idx = sql.toLowerCase().lastIndexOf(" order ");
+		if(idx != -1)
+			sql = "select count(*) n, '1' placeholder from ( " + sql.substring(0, idx) + ") ___t___";
+		return sql;
+	}
+
+	// 拿到sql字符串
+	private String getSql(String str) {
+		String sql = null;
+		try {
+			String path = this.getClass().getClassLoader().getResource("/sqls")
+					.getPath();
+			path += str;
+			RandomAccessFile file = new RandomAccessFile(path, "r");
+			byte[] b = new byte[(int) file.length()];
+			file.read(b);
+			file.close();
+			sql = new String(b);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		return sql;
+	}
+
+	// 把字符串转换成可执行sql
+	private String getExecSql(String sql, JSONObject params) {
+		Program prog = new Program(sql);
+		// 解析
+		Delegate d = prog.parse();
+		Object result = d.invoke();
+		return result.toString();
+	}
+}
